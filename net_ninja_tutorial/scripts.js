cb5eceb9ce5e3131eef77f0ec9c4032c9ca01dbd
tminus109 <martontibor123@gmail.com>
2022-05-25 20:19:52 +0200

    feat: arrays

--- net_ninja_tutorial/scripts.js
+++ net_ninja_tutorial/scripts.js
@@ -89,3 +89,25 @@ for (i = 0; i < tagsArray.length; i++) {
 }
 
 console.log(tagsArrayTrimmed); */
+
+/* var myArray = [];
+myArray[0] = 25;
+myArray[1] = 35;
+myArray[2] = true;
+myArray[3] = "hello";
+
+console.log(myArray);
+
+myArray[2] = false;
+
+console.log(myArray);
+
+var myArray2 = [10, 20, "hi", false];
+
+console.log(myArray2);
+
+var myArray3 = new Array();
+var myArray4 = new Array(5);
+
+console.log(myArray2.sort());
+console.log(myArray2.reverse()); */
