033dd0541243a5c681cb32087c619363469d8190
tminus109 <martontibor123@gmail.com>
2022-05-25 16:17:30 +0200

    feat: loops

---
+++ net_ninja_tutorial/scripts.js
@@ -0,0 +1,39 @@
+// alert("Hello, JavaScript!");
+
+/* var myNum = 11;
+
+if (myNum === 10) {
+    document.write("myNum is equal to 10.");
+} else {
+    document.write("myNum is not equal to 10.");
+} */
+
+/* var myAge = 21;
+
+if (myAge > 30) {
+  document.write("You are over 30.");
+} else if (myAge > 20) {
+  document.write("You are over 20.");
+} else {
+  document.write("You are younger than 20.");
+} */
+
+/* for (i = 0; i < 10; i++) {
+  console.log("Hi!!");
+} */
+
+/* var links = document.getElementsByTagName("a");
+
+for (i = 0; i < links.length; i++) {
+  if (i === 2) {
+    continue;
+  } else if (i === 4) {
+    console.log("Break.");
+    break;
+  }
+  console.log("This is link # " + i);
+}
+
+for (i = 0; i < links.length; i++) {
+  links[i].className = "link-" + i;
+} */
