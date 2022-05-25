cfe66dffc5500138068ce6e86fff4b7f026fafeb
tminus109 <martontibor123@gmail.com>
2022-05-25 20:07:09 +0200

    feat: numbers, strings

--- net_ninja_tutorial/scripts.js
+++ net_ninja_tutorial/scripts.js
@@ -46,3 +46,46 @@ for (i = 0; i < links.length; i++) {
 myResult = getAverage(7, 12);
 console.log("myResult = " + myResult); */
 
+/* var a = "apple";
+var b = 5;
+
+if (isNaN(a)) {
+  console.log("This is not a number!");
+} else {
+  console.log(a * b);
+} */
+
+/* var myString = 'I\'m a "fun" string';
+
+console.log(myString);
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.indexOf("string"));
+console.log(myString.indexOf("ninja"));
+
+if (myString.indexOf("ninja") === -1) {
+  console.log("The word ninja is not in the string.");
+} */
+
+/* var string1 = "abc";
+var string2 = "bcd";
+var string3 = "ABC";
+
+console.log(string1 === string3);
+console.log(string1 < string2); */
+
+/* var str = "Hello, World!";
+var str2 = str.slice(0, 5);
+
+console.log(str2);
+console.log(str.slice(7)); */
+
+/* var tags = "chicken, beef, pork, fish";
+var tagsArray = tags.split(",");
+var tagsArrayTrimmed = [];
+
+for (i = 0; i < tagsArray.length; i++) {
+  tagsArrayTrimmed.push(tagsArray[i].trim());
+}
+
+console.log(tagsArrayTrimmed); */
