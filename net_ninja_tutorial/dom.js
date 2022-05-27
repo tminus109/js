1a338385fc621523dba8bdcd1ca2eb9ff1db5a63
tminus109 <martontibor123@gmail.com>
2022-05-27 12:42:11 +0200

    feat: dom.js, events.js

---
+++ net_ninja_tutorial/dom.js
@@ -0,0 +1,43 @@
+/* document.getElementsByTagName();
+document.getElementsByClassName();
+document.getElementById();
+
+var myBody = document.getElementsByTagName("body");
+
+myBody[0].innerHTML = "<p>New HTML.</p>";
+
+var myTitle = document.getElementById("title");
+
+myTitle.textContent;
+myTitle.textContent = "Hi!"; */
+
+/* var link = document.getElementById("id");
+
+link.getAttribute("href");
+link.setAttribute("href", "/link");
+link.setAttribute("alt", "hello");
+link.href;
+link.style; */
+
+/* var title = document.getElementById("title");
+
+title.setAttribute("style", "color: black; position: relative;");
+title.style.color = "red";
+title.style.backgroundColor = "blue"; */
+
+/* var newLi = document.createElement("li");
+var newA = document.createElement("a");
+var menuUl = document.getElementById("navId").getElementsByTagName("ul")[0];
+
+menuUl.appendChild(newLi);
+newLi.appendChild(newA);
+newA.innerHTML = "Blog";
+
+menuUl.insertBefore(newLi, menuUl.getElementsByTagName("li")[0]); */
+
+/* var parent = document.getElementById("navId").getElementsByTagName("ul")[0];
+var child = parent.getElementsByTagName("li")[0];
+var removed = parent.removeChild(child);
+
+parent.appendChild(removed); */
+
