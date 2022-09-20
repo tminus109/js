7edf1de39a2bdb570116ddd1878eb2c5ffc7eb51
tminus109 <martontibor123@gmail.com>
2022-09-20 17:55:09 +0200

    Mapquest exercise complete

---
+++ mozilla/mapquest/script.js
@@ -0,0 +1,21 @@
+L.mapquest.key = "KEY";
+
+const map = L.mapquest.map("map", {
+  center: [53.480759, -2.242631],
+  layers: L.mapquest.tileLayer("map"),
+  zoom: 12,
+});
+
+map.addControl(L.mapquest.control({ position: "bottomright" }));
+
+L.marker([53.480759, -2.242631], {
+  icon: L.mapquest.icons.marker({
+    primaryColor: "#22407F",
+    secondaryColor: "#3B5998",
+    shadow: true,
+    size: "md",
+    symbol: "A",
+  }),
+})
+  .bindPopup("This is Manchester!")
+  .addTo(map);
