2cd5b92614e7c9eb1f77bf324ae87694ea2d9227
tminus109 <martontibor123@gmail.com>
2022-09-23 18:41:48 +0200

    feat: drew with a loop

--- mozilla/canvas_start/script.js
+++ mozilla/canvas_start/script.js
@@ -80,8 +80,37 @@ ctx.fillRect(0, 0, width, height);
 
 // images
 
-const image = new Image();
-image.src = "firefox.png";
-image.addEventListener("load", () => {
-  ctx.drawImage(image, 20, 20, 185, 175, 50, 50, 185, 175);
-});
+// const image = new Image();
+// image.src = "firefox.png";
+// image.addEventListener("load", () => {
+//   ctx.drawImage(image, 20, 20, 185, 175, 50, 50, 185, 175);
+// });
+
+// loops
+
+// ctx.translate(width / 2, height / 2);
+
+// function degToRad(degrees) {
+//   return (degrees * Math.PI) / 180;
+// }
+
+// function rand(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// let length = 250;
+// let moveOffset = 20;
+
+// for (let i = 0; i < length; i++) {
+//   ctx.fillStyle = `rgba(${255 - length},0,${255 - length},0.9)`;
+//   ctx.beginPath();
+//   ctx.moveTo(moveOffset, moveOffset);
+//   ctx.lineTo(moveOffset + length, moveOffset);
+//   const triHeight = (length / 2) * Math.tan(degToRad(60));
+//   ctx.lineTo(moveOffset + length / 2, moveOffset + triHeight);
+//   ctx.lineTo(moveOffset, moveOffset);
+//   ctx.fill();
+//   length--;
+//   moveOffset += 0.7;
+//   ctx.rotate(degToRad(5));
+// }
