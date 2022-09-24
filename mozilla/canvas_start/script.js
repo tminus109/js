d9d86ba8f417bb02086a858ce7170f3e01a6bdb2
tminus109 <martontibor123@gmail.com>
2022-09-24 11:41:57 +0200

    feat: drawing app complete

--- mozilla/canvas_start/script.js
+++ mozilla/canvas_start/script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector(".myCanvas");
 const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+// const height = (canvas.height = window.innerHeight);
+const height = (canvas.height = window.innerHeight - 85);
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "rgb(0, 0, 0)";
@@ -148,3 +149,56 @@ ctx.fillRect(0, 0, width, height);
 
 //   window.requestAnimationFrame(draw);
 // }
+
+// drawing app
+
+const colorPicker = document.querySelector('input[type="color"]');
+const sizePicker = document.querySelector('input[type="range"]');
+const output = document.querySelector(".output");
+const clearBtn = document.querySelector("button");
+
+function degToRad(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+sizePicker.addEventListener(
+  "input",
+  () => (output.textContent = sizePicker.value)
+);
+
+let curX;
+let curY;
+let pressed = false;
+
+document.addEventListener("mousemove", (e) => {
+  curX = e.pageX;
+  curY = e.pageY;
+});
+
+canvas.addEventListener("mousedown", () => (pressed = true));
+
+canvas.addEventListener("mouseup", () => (pressed = false));
+
+clearBtn.addEventListener("click", () => {
+  ctx.fillStyle = "rgb(0,0,0)";
+  ctx.fillRect(0, 0, width, height);
+});
+
+function draw() {
+  if (pressed) {
+    ctx.fillStyle = colorPicker.value;
+    ctx.beginPath();
+    ctx.arc(
+      curX,
+      curY - 85,
+      sizePicker.value,
+      degToRad(0),
+      degToRad(360),
+      false
+    );
+    ctx.fill();
+  }
+  requestAnimationFrame(draw);
+}
+
+draw();
